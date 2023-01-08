545a33a46888a2eb6ab09e41258b54e39621dec0
Tushar Gugnani <tushar.gugnani@gmail.com>
2023-01-08 22:00:35 +0530

    fix js

--- resources/dist/js/smart-ads.js
+++ resources/dist/js/smart-ads.js
@@ -6,17 +6,21 @@ fetch('/smart-ads-auto-placements')
       placements.forEach(function(placement){
           var adSelector = document.querySelector(placement.selector);
           if(adSelector){
-            let adBody = "<div id=\"smart-ad\" ad-slug=\""+ad.slug+"\">"+ad.body+"</div>"
+            let adBody = "<div class=\"smart-ad-temp\" ad-slug=\""+ad.slug+"\">"+ad.body+"</div>"
             adSelector.insertAdjacentHTML(placement.position, adBody);
-            document.querySelector('#smart-ad').addEventListener('click', updateClick);
           }
       });
+      var smartAd = document.querySelector('.smart-ad-temp');
+      smartAd.firstChild.setAttribute("ad-slug", ad.slug);
+      smartAd.firstChild.classList.add("smart-ad");
+      document.querySelectorAll(".smart-ad-temp").forEach(EL => EL.replaceWith(...EL.childNodes));
+      document.querySelector('.smart-ad').addEventListener('click', updateClick);
     });
 });
 
 function updateClick(e){
     let token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-     let slug = e.target.getAttribute('ad-slug');
+     let slug = e.target.closest('.smart-ad').getAttribute('ad-slug');
      fetch('/smart-ads-update-clicks', {
             headers: {
               "Content-Type": "application/json",
