ebf93b05e57497af333f68f83a87520e90342350
Tushar Gugnani <tushar.gugnani@gmail.com>
2023-01-24 15:52:33 +0530

    fix test

--- tests/SmartAdAdminTest.php
+++ tests/SmartAdAdminTest.php
@@ -15,7 +15,7 @@ class SmartAdAdminTest extends LaravelSmartAdsTestCase
     /** @test */
     public function it_asserts_user_can_view_ads_on_dashboard(){
         $laravelAds = SmartAd::factory()->count(5)->create();
-        $this->get('/smart-ad-manager')
+        $this->get('/smart-ad-manager/ads')
                 ->assertSee($laravelAds->random()->name);
     }
 
